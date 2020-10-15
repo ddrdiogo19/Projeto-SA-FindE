ccd594b7083120af1e6e0069016ec05773510552
Diogo Ricardo <62312320+ddrdiogo19@users.noreply.github.com>
2020-10-14 22:56:16 -0300

    Create cadastro.js

---
+++ telacadastro/cadastro.js
@@ -0,0 +1,32 @@
+var nomecompleto = document.getElementById("nomecompleto")
+var usuario = document.getElementById("usuario")
+var senha = document.getElementById("senha")
+var email = document.getElementById("email")
+var sexo = document.getElementById("sexo")
+
+var dados = []
+
+function Pessoa(nomecompleto, usuario, senha, email, sexo){
+     
+    this.nomecompleto = nomecompleto 
+    this.usuario = usuario
+    this.senha = senha
+    this.email = email
+    this.sexo = sexo
+  
+}
+
+function Cadastrar(){
+        var armazenados = JSON.parse(localStorage.getItem("Pessoa"))
+        if(armazenados != null){
+        dados.push(armazenados)
+        }
+        add = new Pessoa (nomecompleto.value, usuario.value, senha.value, email.value, sexo.value)
+        dados.push(add)
+        alert("Cadastro efetuado com sucesso!")
+        localStorage.setItem("Pessoa", JSON.stringify(dados))
+        console.log(add)
+    
+
+
+}
