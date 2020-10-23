6e3de557a878e2faaa8e8a923bc8aacab0182ac4
Lara Leão <laraleao@hotmail.com>
2020-10-23 19:47:20 -0300

    mudança botão login/entrar e correção de cores

--- criar-evento/criar_evento.js
+++ criar-evento/criar_evento.js
@@ -1,27 +1,27 @@
-let selectEsporte = document.getElementById("esportesSelect");
-let selectLocal = document.getElementById("local");
-let diaEvento = document.getElementById("dia").valueAsDate;
-let horaEvento = document.getElementById("hora").valueAsNumber;
+// let selectEsporte = document.getElementById("esportesSelect");
+// let selectLocal = document.getElementById("local");
+// let diaEvento = document.getElementById("dia").valueAsDate;
+// let horaEvento = document.getElementById("hora").valueAsNumber;
 
-let eventosCadastrados = []
+// let eventosCadastrados = []
 
-function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
+// function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
      
-    this.selectEsporte = selectEsporte 
-    this.selectLocal = selectLocal
-    this.diaEvento = diaEvento
-    this.horaEvento = horaEvento  
-}
+//     this.selectEsporte = selectEsporte 
+//     this.selectLocal = selectLocal
+//     this.diaEvento = diaEvento
+//     this.horaEvento = horaEvento  
+// }
 
-function cadastrarEvento(){
+// function cadastrarEvento(){
 
-    eventosCadastrados = JSON.parse(localStorage.getItem("Evento")) 
-    add = new Evento (selectEsporte.value, selectLocal.value, diaEvento, horaEvento)
-    eventosCadastrados.push(add)
-    localStorage.setItem("Evento", JSON.stringify(eventosCadastrados))
-    // console.log(add)
-    alert(selectEsporte)
-}
+//     eventosCadastrados = JSON.parse(localStorage.getItem("Evento")) 
+//     add = new Evento (selectEsporte.value, selectLocal.value, diaEvento, horaEvento)
+//     eventosCadastrados.push(add)
+//     localStorage.setItem("Evento", JSON.stringify(eventosCadastrados))
+//     // console.log(add)
+//     alert(selectEsporte)
+// }
 
 
 
@@ -49,3 +49,14 @@ function cadastrarEvento(){
 // var hoje = new Date();
 //   if (agendamento <= hoje) {
 //     alert('Por favor, insira uma data válida!');
+
+// date
+
+// var input = document.getElementById('ultimoDiaTrab');
+// input.addEventListener('change', function() {
+//   var agora = new Date();
+//   var escolhida = new Date(this.value);
+//   if (escolhida < agora) this.value = [agora.getFullYear(), agora.getMonth() + 1, agora.getDate()].map(v => v < 10 ? '0' + v : v).join('-');
+// });
+
+// Neste caso this.value = [agora.getFullYear(), agora.getMonth() + 1, agora.getDate()].map(v => v < 10 ? '0' + v : v).join('-'); repoõe a data escolhida com a data atual, se quiseres somente apagar podes fazer this.value = '';
