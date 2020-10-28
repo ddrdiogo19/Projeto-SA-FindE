03ac743902e82ef08a428259b13a350bb10ab014
ddrdiogo19 <ddrdiogo@hotmail.com>
2020-10-28 15:11:40 -0300

    Ultima atualizacao pagina de relatorio

--- criar-evento/criar_evento.js
+++ criar-evento/criar_evento.js
@@ -1,37 +1,42 @@
-var esporte;
-var local;
-var data;
-var horario;
-var dados = [];
+let esporte; 
+let local;
+let data;
+let horario;
+let dados = []; //  [{"esporte": esporte, "local": local, ...}];
 
 function pegarEsporte() {
-  let selectEsporte = document.getElementById("esportesSelect").value;
+  const selectEsporte = document.getElementById("esportesSelect").value;
 
   esporte = selectEsporte;
   console.log(esporte)
 }
 
 function pegarLocal() {
-  let selectLocal = document.getElementById("local").value;
+  const selectLocal = document.getElementById("local").value;
   
   local = selectLocal
   console.log(local)
 }
 
 function pegarData() {
-  let diaEvento = document.getElementById("dia").valueAsDate;
+  const diaEvento = document.getElementById("dia").value;
   
-  // converter data
-  data = diaEvento
-  console.log(data)
+  // diaEvento = new Date(dateString)
+  console.log(diaEvento);
+
+
 }
 
 function pegarHorario() {
-  let horaEvento = document.getElementById("hora").valueAsNumber;
+  const hora = document.getElementById("inputHora").value;
+  const minuto = document.getElementById("inputMinuto").value;
 
-  // converter de utc
-  horario = horaEvento
+  horario = `${hora}:${minuto}`;
   console.log(horario)
+}
+
+function criarObjeto(){
+
 }
 
 function pegarDadosNoLocalStorage() {
@@ -40,7 +45,8 @@ function pegarDadosNoLocalStorage() {
 
 function salvarDadosNoLocalStorage(){
   pegarDadosNoLocalStorage();
-  localStorage.setItem("Evento", JSON.stringify(dados))
+  //  dados.push(objeto)
+  localStorage.setItem("Evento", JSON.stringify(dados));
 }
 
 function cadastrar(){
@@ -48,57 +54,54 @@ function cadastrar(){
   pegarLocal();
   pegarData();
   pegarHorario();
+  criarObjeto();
   salvarDadosNoLocalStorage();
-
 }
 
-/* let dados = []
-
-
-function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
+/*function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
      
-    this.selectEsporte = selectEsporte 
-    this.selectLocal = selectLocal
-    this.diaEvento = diaEvento
-    this.horaEvento = horaEvento  
-} */
+  this.selectEsporte = selectEsporte 
+  this.selectLocal = selectLocal
+  this.diaEvento = diaEvento
+  this.horaEvento = horaEvento  
+} 
 
-//     dados = JSON.parse(localStorage.getItem("Evento")) 
+dados = JSON.parse(localStorage.getItem("Evento")) 
 
-//        if (dados == null){
+       if (dados == null){
 
-//         dados = []
-//         add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
-//         dados.push(add)
-//         localStorage.setItem("Evento", JSON.stringify(dados))
-//         alert ("Seu evento foi cadastrado com sucesso")
+        dados = []
+        add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
+        dados.push(add)
+        localStorage.setItem("Evento", JSON.stringify(dados))
+         alert ("Seu evento foi cadastrado com sucesso")
 
-//     } else{
+     } else{
 
-//         add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
-//         dados.push(add)
-//         localStorage.setItem("Evento", JSON.stringify(dados))
-//         alert ("Seu evento foi cadastrado com sucesso")
+        add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
+         dados.push(add)
+         localStorage.setItem("Evento", JSON.stringify(dados))
+        alert ("Seu evento foi cadastrado com sucesso")
 
-//     }} 
-//   }
+     }} 
+  }
 
 
 
-    //     dados = []
-    //     add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
-    //     dados.push(add)
-    //     localStorage.setItem("Evento", JSON.stringify(dados))
-    //     alert ("Seu evento foi cadastrado com sucesso")
+       dados = []
+       add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
+       dados.push(add)
+       localStorage.setItem("Evento", JSON.stringify(dados))
+       alert ("Seu evento foi cadastrado com sucesso")
 
-    // } else{
+   } else{
 
-    //     add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
-    //     dados.push(add)
-    //     localStorage.setItem("Evento", JSON.stringify(dados))
-    //     alert ("Seu evento foi cadastrado com sucesso")
+       add = new Evento (selectEsporte, selectLocal, diaEvento, horaEvento)
+       dados.push(add)
+       localStorage.setItem("Evento", JSON.stringify(dados))
+       alert ("Seu evento foi cadastrado com sucesso")
 
-    // }} 
+   }} */ 
 
 // function pular {
 
@@ -106,12 +109,12 @@ function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
 // }
 
 // // function editar(){
-    
+  
 //     eventosCadastrados[indicepesquisa].selectEsporte = selectEsporte.value
 //     eventosCadastrados[indicepesquisa].selectLocal = selectLocal.value
 //     eventosCadastrados[indicepesquisa].diaEvento = diaEvento.value
 //     eventosCadastrados[indicepesquisa].horaEvento = horaEvento.value
-   
+ 
 
 //     swal("Dados atualizados com sucesso!")
 //     localStorage.setItem("Evento", JSON.stringify(dados))
@@ -123,7 +126,7 @@ function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
 
 //    let exclusao = selectEsporte.value
 //    dados = JSON.parse(localStorage.getItem("dados"))
-      
+    
 //  for(i=0; i < dados.length; i++){
 
 //       if(dados[i].esportesSelect == exclusao) {
@@ -138,31 +141,5 @@ function Evento (selectEsporte, selectLocal, diaEvento, horaEvento){
 //   localStorage.setItem("Evento", JSON.stringify(armazenados))
 
 // }
-
-// let dados = []
-
-
-
-// date 
-
-// var today = new Date(); //Gravo a data atual na variavel
-// today.setDate(today.getDate() + 7); //Adiciono 7 dias 
-// today = today.toISOString().split('T')[0]; //Formato a data
-
-// var hoje = new Date();
-//   if (agendamento <= hoje) {
-//     alert('Por favor, insira uma data válida!');
-
-// date
-
-// var input = document.getElementById('ultimoDiaTrab');
-// input.addEventListener('change', function() {
-//   var agora = new Date();
-//   var escolhida = new Date(this.value);
-//   if (escolhida < agora) this.value = [agora.getFullYear(), agora.getMonth() + 1, agora.getDate()].map(v => v < 10 ? '0' + v : v).join('-');
-// });
-
-// Neste caso this.value = [agora.getFullYear(), agora.getMonth() + 1, agora.getDate()].map(v => v < 10 ? '0' + v : v).join('-'); repoõe a data escolhida com a data atual, se quiseres somente apagar podes fazer this.value = '';
-
 
 
