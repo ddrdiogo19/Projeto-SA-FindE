0d68ec88b1d1c757abaa1df252989134e7d2d42b
Lara Leão <laraleao@hotmail.com>
2020-11-03 22:13:13 -0300

    finalização página de relatório

--- relatorio/relatorio_de_eventos.js
+++ relatorio/relatorio_de_eventos.js
@@ -1,19 +1,23 @@
 function Eventos() {
 
+  const inputLocal = document.getElementById("local").value;
+
   let listacompleta = ''
-    dados = JSON.parse(localStorage.getItem("Evento"))
-  
+    dados = JSON.parse(localStorage.getItem("Evento"));
+
         for (i=0; i < dados.length; i++){
+          const esporte = dados[i].esporte;
+          const local = dados[i].local;
+          const dataFormatada = dados[i].dataFormatada;
+          const horario = dados[i].horario;
+          
+  if (local == inputLocal) {
+    const evento = `Esporte: ${esporte}, Local: ${local}, Dia: ${dataFormatada}, Hora: ${horario}`
   
-            listacompleta += (Object.values(dados[i]).join(' , ')) + '<br>'
-            // i = local.value
-          }
-  
+    listacompleta += evento + '<br>'
+    }
+}
         document.getElementById("resposta").innerHTML = listacompleta
-    
+
   } 
-  
-
-  // ter que fazer if, perguntando do dado - select 
-  // comparar select com localStorage
-
+   
