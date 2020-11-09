04b96bd7a5c253d52cfe4bb424f09e4f799b64dc
Lara Leão <laraleao@hotmail.com>
2020-11-09 18:49:26 -0300

    página de login

--- login/teladelogin.js
+++ login/teladelogin.js
@@ -1,19 +1,30 @@
+const element = document.querySelector("form");
+const data = JSON.parse(localStorage.getItem("Pessoa"));
+let isAuthorized = false;
+
+element.addEventListener("submit", (event) => {
+  //    previne atualização da página
+  event.preventDefault();
+  login();
+});
+
+//  função que recebe login e senha como parametros e busca eles no array de logins
+function validateLogin(username, password) {
+  data.find((user, index) => {
+    if (user.usuario == username && user.senha == password) {
+      isAuthorized = true;
+      console.log("Login authorized!");
+    }
+  });
+}
+
 function login() {
+  const user = document.getElementById("usuario").value;
+  const password = document.getElementById("senha").value;
 
-    var user = document.getElementById("usuario").value;
-    var password = document.getElementById("senha").value;
+  validateLogin(user, password);
 
-
-    let logins_index = [];
-    let senhas_index = [];
-
-    dar Push
-    localStorage
-
-    puxar localStorage
-
-    // não esquecer validação esqueceu a sua senha // 
- }
-
-
+  if (isAuthorized)
+    document.location.href = "../relatorio/relatorio_de_eventos.html";
+  else alert("Usuário ou senha inválidos");
 }
--- relatorio/relatorio_de_eventos.js
+++ relatorio/relatorio_de_eventos.js
@@ -1,23 +1,20 @@
 function Eventos() {
-
   const inputLocal = document.getElementById("local").value;
 
-  let listacompleta = ''
-    dados = JSON.parse(localStorage.getItem("Evento"));
+  let listacompleta = "";
+  dados = JSON.parse(localStorage.getItem("Evento"));
 
-        for (i=0; i < dados.length; i++){
-          const esporte = dados[i].esporte;
-          const local = dados[i].local;
-          const dataFormatada = dados[i].dataFormatada;
-          const horario = dados[i].horario;
-          
-  if (local == inputLocal) {
-    const evento = `Esporte: ${esporte}, Local: ${local}, Dia: ${dataFormatada}, Hora: ${horario}`
-  
-    listacompleta += evento + '<br> <br>'
+  for (i = 0; i < dados.length; i++) {
+    const esporte = dados[i].esporte;
+    const local = dados[i].local;
+    const dataFormatada = dados[i].dataFormatada;
+    const horario = dados[i].horario;
+
+    if (local == inputLocal) {
+      const evento = `Esporte: ${esporte}, Local: ${local}, Dia: ${dataFormatada}, Hora: ${horario}`;
+
+      listacompleta += evento + "<br> <br>";
     }
+  }
+  document.getElementById("resposta").innerHTML = listacompleta;
 }
-        document.getElementById("resposta").innerHTML = listacompleta
-
-  } 
-   
