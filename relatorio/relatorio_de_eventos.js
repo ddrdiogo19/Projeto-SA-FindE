66a05f890f947b07e07f3137c21065180c16a179
Lara Leão <laraleao@hotmail.com>
2020-11-09 22:56:06 -0300

    melhoramentos

--- relatorio/relatorio_de_eventos.js
+++ relatorio/relatorio_de_eventos.js
@@ -3,30 +3,29 @@ var dados = JSON.parse(localStorage.getItem("Evento"));
 
 function Eventos() {
   const inputLocal = document.getElementById("local").value;
+  if (inputLocal == "--Selecione a opção") return;
 
   tabela.innerHTML = "";
 
-  if (inputLocal != "--Selecione a opção") {
-    var header = document.createElement("tr");
+  var header = document.createElement("tr");
 
-    var th1 = document.createElement("th");
-    th1.textContent = "Esporte";
-    header.append(th1);
+  var th1 = document.createElement("th");
+  th1.textContent = "Esporte";
+  header.append(th1);
 
-    var th2 = document.createElement("th");
-    th2.textContent = "Local";
-    header.append(th2);
+  var th2 = document.createElement("th");
+  th2.textContent = "Local";
+  header.append(th2);
 
-    var th3 = document.createElement("th");
-    th3.textContent = "Data";
-    header.append(th3);
+  var th3 = document.createElement("th");
+  th3.textContent = "Data";
+  header.append(th3);
 
-    var th4 = document.createElement("th");
-    th4.textContent = "Horário";
-    header.append(th4);
+  var th4 = document.createElement("th");
+  th4.textContent = "Horário";
+  header.append(th4);
 
-    tabela.append(header);
-  }
+  tabela.append(header);
 
   for (var i = 0; i < dados.length; i++) {
     const evento = dados[i];
